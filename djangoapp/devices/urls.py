770821fb0e38fa2c363012184183936a58d8162f
realdanvanth <realdanvanth@gmail.com>
2025-04-29 16:17:37 +0530

    Delete djangoapp directory

--- djangoapp/devices/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from devices import views
-
-urlpatterns = [
-    path('serial/devices/', views.list_devices, name='list_devices'),
-    path('serial/data/<str:port>/', views.get_serial_data, name='get_serial_data'),
-    path('serial/data/view/<str:port>/', views.serial_data_view, name='serial_data_view'),
-]
